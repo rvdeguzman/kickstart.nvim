f939a47fb08f75045312b5b2a92c9439f0252605
rv <rv@komagome.local>
2025-06-14 21:18:49 -0400

    hl on yank

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 -- Split window mappings
