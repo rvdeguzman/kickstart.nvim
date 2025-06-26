be319d399c1c34949514a080a6cedd0cc85304b6
rvdeguzman <rvdeguzman@protonmail.com>
2025-06-25 20:13:45 -0400

    added to mini

--- lua/rv/plugins/mini.lua
+++ lua/rv/plugins/mini.lua
@@ -17,20 +17,13 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- Add/remove/navigate using ]q, [q, ]b, [b, ]t, [t, etc.
+      require('mini.bracketed').setup()
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- Fast and familiar per-line commenting
+      -- gcc - comment/uncomment line
+      -- gc - comment/uncomment selection
+      require('mini.comment').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
