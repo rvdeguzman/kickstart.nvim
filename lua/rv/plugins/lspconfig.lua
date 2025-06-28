597c396fc07519c8561d5ebcded162d343d096fa
rvdeguzman <rvdeguzman@protonmail.com>
2025-06-27 20:37:28 -0400

    removed jdtls

--- lua/rv/plugins/lspconfig.lua
+++ lua/rv/plugins/lspconfig.lua
@@ -223,7 +223,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        -- 'jdtls',
         'clangd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
