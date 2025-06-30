8ee43de0bbf7714046e4c5c24b57fee3602b5fe4
rvdeguzman <rvdeguzman@protonmail.com>
2025-06-30 09:36:25 -0400

    copilot binds

--- lua/rv/plugins/copilot.lua
+++ lua/rv/plugins/copilot.lua
@@ -5,13 +5,14 @@ return {
   event = 'BufReadPost',
   opts = {
     suggestion = {
-      enabled = not vim.g.ai_cmp,
+      enabled = true,
       auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
+      hide_during_completion = true,
       keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
+        accept = '<C-]>',
         next = '<M-]>',
         prev = '<M-[>',
+        dismiss = '<C-e>',
       },
     },
     panel = { enabled = false },
