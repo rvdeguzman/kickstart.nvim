98842773ec25ffac4ed4aa583eb06cde2e4ac26a
rvdeguzman <rvdeguzman@protonmail.com>
2025-07-10 13:58:34 -0400

    added harpoon telescope

--- lua/rv/plugins/harpoon.lua
+++ lua/rv/plugins/harpoon.lua
@@ -26,6 +26,33 @@ return {
         end,
         desc = 'Harpoon Quick Menu',
       },
+      {
+        '<leader>ht',
+        function()
+          local harpoon = require 'harpoon'
+          local conf = require('telescope.config').values
+          local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+            end
+
+            require('telescope.pickers')
+              .new({}, {
+                prompt_title = 'Harpoon',
+                finder = require('telescope.finders').new_table {
+                  results = file_paths,
+                },
+                previewer = conf.file_previewer {},
+                sorter = conf.generic_sorter {},
+              })
+              :find()
+          end
+
+          toggle_telescope(harpoon:list())
+        end,
+        desc = 'Open harpoon window with Telescope',
+      },
     }
 
     for i = 1, 9 do
